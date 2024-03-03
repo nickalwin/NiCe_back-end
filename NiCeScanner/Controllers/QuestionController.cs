e1e5af08d80eb86dadab701652b1d6cace248998
Tycjan-Fortuna-IT <242213@edu.p.lodz.pl>
2024-03-03 11:36:46 +0100

    Questions index endpoint

---
+++ NiCeScanner/Controllers/QuestionController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NiCeScanner.Data;
+using NiCeScanner.Models;
+
+namespace NiCeScanner.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class QuestionController : Controller
+	{
+		private readonly ApplicationDbContext _context;
+
+		public QuestionController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<Question>>> GetQuestions()
+		{
+			return await _context.Questions.ToListAsync();
+		}
+	}
+}
