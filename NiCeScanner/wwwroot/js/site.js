007d403a13c1898f8ff431377e6c20b320c1659a
nickalwin <nick.alwin.oost@gmail.com>
2024-03-18 13:23:08 +0100

    navbar changed and finished login

--- NiCeScanner/wwwroot/js/site.js
+++ NiCeScanner/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener('DOMContentLoaded', function () {
+    var links = document.querySelectorAll('.nav-link');
+    var currentPageUrl = window.location.href;
+
+    links.forEach(function (link) {
+        var linkUrl = link.getAttribute('href');
+
+        if (currentPageUrl.includes(linkUrl) && linkUrl !== '/') {
+            link.parentElement.classList.add('active');
+        }
+    });
+});
